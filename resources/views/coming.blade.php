02b8f44d6b30ebdddeb04b96a73a854f6a78e8d1
Douglas Cockerham <douglascockerham@polk-county.net>
2021-09-10 10:48:14 -0400

    Initial commit

---
+++ resources/views/coming.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Polk HealthCare Plan</title>
+
+    <!-- Favicon -->
+    <link href="favicon.ico" rel="icon" />
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <!-- Stylesheets -->
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+<style type="text/css">
+    html, body {
+        font-family: "Inter", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    }
+</style>
+
+<div class="bg-gray-900 min-h-screen px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+    <div class="max-w-max mx-auto">
+        <main class="sm:flex">
+            <div class="lg:grid lg:grid-cols-2 lg:gap-8">
+                <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
+                    <div class="lg:py-24">
+                        <span class="inline-flex items-center text-white px-3 py-0.5 text-xs sm:text-base lg:text-sm xl:text-base font-semibold leading-5 uppercase tracking-wide bg-indigo-500 rounded-full">Coming Soon</span>
+                        <h1 class="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+                            <span class="block">A Reimagined</span>
+                            <span class="block text-indigo-400">Polk HealthCare Plan</span>
+                        </h1>
+                        <p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                            We're working hard to build a personalized experience for you.
+                        </p>
+                    </div>
+                </div>
+                <div class="mt-12 lg:m-0 lg:relative">
+                    <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0">
+                        <img class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto" src="https://cdn.polk.design/images/undraw-work-together.svg" aria-label="People working together to build a website">
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</div>
+</body>
+</html>
